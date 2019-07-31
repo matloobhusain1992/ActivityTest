ebf589b3cdf676f8d8a408045c3f1812c5cf6c24
matloobhusain1992 <matloobhusain1992@gmail.com>
2019-07-31 17:18:36 +0530

    feature1

--- app/src/main/java/jagran/zmq/com/activitytest/activity_flag/A.java
+++ app/src/main/java/jagran/zmq/com/activitytest/activity_flag/A.java
@@ -35,6 +35,7 @@ public class A extends AppCompatActivity {
         System.out.println("child added rahil");
         System.out.println("master added sahil");
         System.out.println("child  sahil");
+        System.out.println("feature 1");
     }
 
     @Override
