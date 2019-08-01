e4b29b3c1a73429f036944a8a4ff9409528b4f20
matloobhusain1992 <matloobhusain1992@gmail.com>
2019-08-01 18:25:02 +0530

    m3

--- app/src/main/java/jagran/zmq/com/activitytest/activity_flag/A.java
+++ app/src/main/java/jagran/zmq/com/activitytest/activity_flag/A.java
@@ -36,6 +36,7 @@ public class A extends AppCompatActivity {
         System.out.println("Child 1");
         System.out.println("master 2");
         System.out.println("Child 2");
+        System.out.println("master 3");
     }
 
     @Override
